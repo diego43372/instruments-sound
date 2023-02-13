3631e8f92b36b2c77036377fe54982fff6422c88
Nicolas Triana <109915700+nicolastriana88@users.noreply.github.com>
2023-02-12 22:15:18 -0500

    Add files via upload

---
+++ vistas/iniciar sesion/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar Sesion</title>
+    <link rel="stylesheet" href="style-ini.css">
+    <script src="https://kit.fontawesome.com/3308f2daf1.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<form action="IniciarSesion.php" method="post">
+<label>Correo</label>
+<input type="text" name="correo" placeholder="correo">
+<label>Contraseña</label>
+<input type="text" name="contraseña" placeholder="contraseña">
+<buttom type="submit">Iniciar sesion</buttom>
+<a href="http://localhost/bases%20de%20datos%20instruments%20sound/pagina1.php">Registrar usuario</a>
+</form>
+</body>
+</html>
---
+++ vistas/registrarse/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instruments sound register</title>
+    <link rel="stylesheet" href="style-regi.css">
+</head>
+<style>
+    body{
+        background-color: #6A5ACD
+       
+            }
+          
+h1{
+    font-family: verdana;
+    font-size:42px;
+    text-align: center;
+    color:white;
+}
+
+
+ </style>   
+<body>
+    <h1> Registro usuarios Instruments Sound </h1>
+    <div class="formulario">
+    <form action="registro.php" method="post">
+        
+        Identificacion:
+        <input type="text" name="identificacion"> <br>
+        Correo:
+        <input type="text" name="correo"><br>
+        Nombre:
+        <input type="text" name="nombre"><br>
+        apellidos:
+        <input type="text" name="apellidos"><br>
+        Contraseña:
+        <input type="text" name="contraseña"><br>
+        Estado:
+        <input type="text" name="estado"><br>
+        Rol:
+        <input type="text" name="rol"><br>
+
+        
+       
+        
+        <input type="submit" value="Registrar">
+        </div>
+    </from>
+</body>
+</html>
